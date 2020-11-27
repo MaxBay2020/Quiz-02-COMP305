71ba82cc49937e5875af41c836012cb0d6689694
Max Bay <cwang167@my.centennialcollege.ca>
2020-11-27 15:47:48 -0500

    Quiz 02 for COMP 305
    Quiz 02 assignment

---
+++ Assets/Scripts/BGSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSwitch : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public Transform items;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayBG()
+    {
+        Camera.main.GetComponent<AudioSource>().enabled = true;
+    }
+
+    public void DestoryItems()
+    {
+        Destroy(items);
+    }
+}
